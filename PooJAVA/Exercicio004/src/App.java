0baa51a0e63432f628b9983095168ce89b99d00c
Andre Ferreira <aferreira1535@gmail.com>
2022-07-20 16:52:47 -0300

    exer04

--- PooJAVA/Exercicio004/src/App.java
+++ PooJAVA/Exercicio004/src/App.java
@@ -1,3 +1,8 @@
+/*Crie uma classe denominada Elevador para armazenar as informações de um
+elevador dentro de um prédio. A classe deve armazenar o andar atual (térreo = 0),
+total de andares no prédio (desconsiderando o térreo), capacidade do elevador e
+quantas pessoas estão presentes nele. A classe deve também disponibilizar os
+seguintes métodos: */
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
